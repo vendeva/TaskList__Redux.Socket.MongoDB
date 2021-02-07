b835ca64c7a34fe0e6a7eef3b221bc61e46a334b
Vendeva <v190584@mail.ru>
2021-02-08 00:12:46 +0500

    add debug

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+
+//logging
+const httpInfo = require("debug")("connect:http"),
+    name = "TaskList";
+const socketInfo = require("debug")("connect:socket");
+const mongoInfo = require("debug")("connect:mongoInfo");
+const mongoError = require("debug")("connect:mongoError");
 
 const todoModel = require("./models/todoModel"); //todo model
 
@@ -12,35 +18,44 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
-http.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+http.listen(PORT, () => httpInfo(`Server is listening on port ${PORT}`));
 
-app.get("/", (request, response) => {
-    response.send("I'm server");
+httpInfo("booting %o", name);
+
+app.get("/", (req, res) => {
+    httpInfo(req.method + " " + req.url);
+    res.send("I'm server");
 });
 
-app.use((req, res, next) => {
+app.use((req, res) => {
+    httpInfo(req.method + " 404");
     res.status(404).send("404 The page does not exist!");
 });
 
-mongoose.connect("mongodb://localhost/local");
+mongoose.connect("mongodb://localhost/local", {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
 
 var db = mongoose.connection;
 db.on("error", () => {
-    console.log("Failed to connect to mongoose");
+    mongoError("Failed to connect to mongoose");
 });
 db.once("open", () => {
-    console.log("Connected to mongoose");
+    mongoInfo("Connected to mongoose");
 });
 
 io.on("connection", function (socket) {
-    console.log("Connected to Socket" + socket.id);
+    socketInfo("Connected to Socket" + socket.id);
     socket.on("disconnect", function () {
-        console.log("Disconnected - " + socket.id);
+        socketInfo("Disconnected - " + socket.id);
     });
 
     todoModel.find({}, "-_id parentId id text completed", { lean: true }, (err, result) => {
         if (err) {
-            console.log("GET TODOS failed!" + err);
+            mongoError("GET TODOS failed! " + err);
         } else {
             let todos = result.filter((todo) => !todo.parentId);
             if (todos) {
@@ -58,7 +73,7 @@ io.on("connection", function (socket) {
                 }));
             }
             socket.emit("initialList", todos);
-            console.log({ message: "GET TODOS worked!" });
+            mongoInfo("GET TODOS worked!");
         }
     });
 
@@ -71,11 +86,11 @@ io.on("connection", function (socket) {
 
         todo.save((err, result) => {
             if (err) {
-                console.log("ADD NEW Todo failed!" + err);
+                mongoError("ADD NEW Todo failed! " + err);
             } else {
                 const { parentId, id, text } = result;
                 io.emit("todoAdded", parentId, id, text);
-                console.log({ message: "ADD NEW Todo worked!" });
+                mongoInfo("ADD NEW Todo worked!");
             }
         });
     });
@@ -86,10 +101,10 @@ io.on("connection", function (socket) {
 
         todoModel.updateOne(query, update, (err) => {
             if (err) {
-                console.log("EDIT Todo failed! " + err);
+                mongoError("EDIT Todo failed! " + err);
             } else {
                 io.emit("todoEdited", id, text, parentId);
-                console.log({ message: "EDIT Todo worked!" });
+                mongoInfo("EDIT Todo worked!");
             }
         });
     });
@@ -99,25 +114,36 @@ io.on("connection", function (socket) {
         const update = { completed };
 
         todoModel.updateOne(query, update, (err, result) => {
-            if (err) throw err;
-            io.emit("todoCompleted", id, parentId);
-            console.log({ message: "COMPLETE Todo worked!" });
+            if (err) {
+                mongoError("COMPLETE Todo failed!" + err);
+            } else {
+                io.emit("todoCompleted", id, parentId);
+                mongoInfo("COMPLETE Todo worked!");
 
-            todoModel.find({ parentId }, "-_id parentId completed", (err, result) => {
-                if (err) throw err;
-                const allSubTodosCompleted = result.some((item) => item.completed === false);
-                if (!allSubTodosCompleted) {
-                    todoModel.updateOne(
-                        { id: parentId },
-                        { completed: true },
-                        (err, resultParent) => {
-                            if (err) throw err;
-                            io.emit("todoCompleted", parentId, 0);
-                            console.log({ message: "COMPLETE Todo worked!" });
+                todoModel.find({ parentId }, "-_id parentId completed", (err, result) => {
+                    if (err) {
+                        mongoError("FIND Todo failed!" + err);
+                    } else {
+                        const allSubTodosCompleted = result.some(
+                            (item) => item.completed === false
+                        );
+                        if (!allSubTodosCompleted) {
+                            todoModel.updateOne(
+                                { id: parentId },
+                                { completed: true },
+                                (err, resultParent) => {
+                                    if (err) {
+                                        mongoError("COMPLETE Todo failed!" + err);
+                                    } else {
+                                        io.emit("todoCompleted", parentId, 0);
+                                        mongoInfo("COMPLETE Todo worked!");
+                                    }
+                                }
+                            );
                         }
-                    );
-                }
-            });
+                    }
+                });
+            }
         });
     });
 
@@ -126,17 +152,17 @@ io.on("connection", function (socket) {
 
         todoModel.deleteOne(query, (err) => {
             if (err) {
-                console.log("DELETE Todo failed! " + err);
+                mongoError("DELETE Todo failed! " + err);
             } else {
                 io.emit("todoDeleted", id, parentId);
-                console.log({ message: "DELETE Todo worked!" });
+                mongoInfo("DELETE Todo worked!");
                 if (!parentId) {
                     const query = { parentId: id };
                     todoModel.deleteMany(query, (err) => {
                         if (err) {
-                            console.log("DELETE SubTodos failed! " + err);
+                            mongoError("DELETE SubTodos failed! " + err);
                         } else {
-                            console.log({ message: "DELETE SubTodos worked!" });
+                            mongoInfo("DELETE SubTodos worked!");
                         }
                     });
                 }
@@ -155,18 +181,18 @@ io.on("connection", function (socket) {
 
         todoModel.deleteMany(query, (err) => {
             if (err) {
-                console.log("DELETE Todos failed! " + err);
+                mongoError("DELETE Todos failed! " + err);
             } else {
-                console.log({ message: "DELETE Todos worked!" });
+                mongoInfo("DELETE Todos worked!");
             }
         });
 
         todoModel.insertMany(todosForDb, (err) => {
             if (err) {
-                console.log("INSERT Todos failed! " + err);
+                mongoError("INSERT Todos failed! " + err);
             } else {
                 io.emit("todoSorted", todos, parentId);
-                console.log({ message: "INSERT Todos worked!" });
+                mongoInfo("INSERT Todos worked!");
             }
         });
     });
@@ -177,10 +203,10 @@ io.on("connection", function (socket) {
 
         todoModel.updateMany({}, update, (err) => {
             if (err) {
-                console.log("COMPLETE All Todos failed! " + err);
+                mongoError("COMPLETE All Todos failed! " + err);
             } else {
                 io.emit("completeAllTodos");
-                console.log({ message: "COMPLETE All Todos worked!" });
+                mongoInfo("COMPLETE All Todos worked!");
             }
         });
     });
@@ -188,10 +214,10 @@ io.on("connection", function (socket) {
     socket.on("clearCompleted", () => {
         todoModel.deleteMany({ completed: true }, (err) => {
             if (err) {
-                console.log("CLEAR COMPLETED Todos failed!" + err);
+                mongoError("CLEAR COMPLETED Todos failed! " + err);
             } else {
                 io.emit("clearCompleted");
-                console.log({ message: "CLEAR COMPLETED worked!" });
+                mongoInfo("CLEAR COMPLETED worked!");
             }
         });
     });
